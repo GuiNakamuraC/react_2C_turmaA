1a005ad76925e3ce7cec2f4e0e6e70413ea636b0
GuiNakamuraC <guincarvalho08@gmail.com>
2024-09-18 19:03:12 -0300

    aula12

--- etec-filmes/src/pages/Home/index.js
+++ etec-filmes/src/pages/Home/index.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, {categories, filterCategory} from "../../components/Category";
@@ -8,34 +7,16 @@ import Header from "../../components/Header";
 
 function Home() {
 
-  // let nomeDigitado=''
-  function pegarNome(event){
-    console.log(event.target.value)
-    // nomeDigitado = event.target.value
-    setNome(event.target.value)
-  }
-
-  // Hook = gancho -> useState
-  const [ nome, setNome ] = useState();
-
   return (
     <>
       <Header/>
       <Banner image="favoritos" />
       <Container>
-
-        <input
-          type="text"
-          placeholder="Nome"
-
-          onChange={pegarNome}
-        />
-        <h2>{nome}</h2>
-        {/* {categories.map((category, index) =>
+        {categories.map((category, index) =>
           <Category category={category}>
             { filterCategory(index).map((video) => <Card id={video.id} key={video.id} />)}
           </Category>
-        )} */}
+        )}
       </Container>      
       <Footer/>
     </>
