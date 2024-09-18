fa90b56bfd124b32409d6256c1feaa40dec33c48
GuiNakamuraC <guincarvalho08@gmail.com>
2024-09-18 19:53:34 -0300

    aula13

--- etec-filmes/src/pages/Search/index.js
+++ etec-filmes/src/pages/Search/index.js
@@ -2,7 +2,7 @@ import styles from "./Search.module.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 import Container from "../../components/Container";
-import VideoList from "../../components/VideoList";
+import SearchVideoList from "../../components/SearchVideoList";
 import videos from "../../json/videos.json"
 
 function Search(){
@@ -12,7 +12,7 @@ function Search(){
         <Container>
             <section className={styles.search}>
                 <h2>Pesquisar</h2>
-                <VideoList videos={videos} />
+                <SearchVideoList videos={videos} />
             </section>
         </Container>
         <Footer />
