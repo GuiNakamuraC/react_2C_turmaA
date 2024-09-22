2a828c32900730a7cb7754987beb1c5257a47345
GuiNakamuraC <guincarvalho08@gmail.com>
2024-09-22 20:46:47 -0300

    aula19

--- etec-filmes/src/components/Form/index.js
+++ etec-filmes/src/components/Form/index.js
@@ -1,32 +1,91 @@
-import { categories } from "../Category";
 import styles from "./Form.module.css";
+import { categories } from "../Category";
+import { useState } from "react";
 
-function Form(){
-    return(
+function Form() {
+
+    const [ url, setUrl ] = useState('')
+    const [ category, setCategory ] = useState('')
+    const [ videos, setVideos ] = useState([])
+    const [ errors, setErrors ] = useState('')
+
+    function valideUrl(url) {
+        const regex = /^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:embed\/|watch\?v=)|youtu\.be\/)([a-zA-Z0-9\-_]+)$/
+    
+        if(!regex.test(url) || url.length < 43) {
+            setErrors('ERRO: URL inválida!')
+            return false
+        } else {
+            return url.substring(32, 43) // ID do vídeo
+        }
+    }
+
+    function onSave(e) {
+        e.preventDefault()
+        console.log(url, category)
+
+        // Validar a category
+        if(!category || category === '-') {
+            console.log('Escolha uma categoria')
+            setErrors('ERRO: Escolha uma categoria!')
+            return
+        } else {
+            setErrors('')
+        }
+
+        // Validar URL
+        const urlVideo = valideUrl(url)
+        if(urlVideo && category) {
+            // Guardar a URL e a category
+            const newVideo = { url, category }
+            setVideos([...videos, newVideo])
+            localStorage.setItem('videos', JSON.stringify([...videos, newVideo]))
+            // Limpar o form
+            setUrl('')
+            setCategory('')
+        } else {
+            setErrors('ERRO: URL inválida!')
+        }
+
+    }
+
+    return (
         <section className={styles.container}>
-            <h2>Cadastro de Vídeo</h2>
-            <form>
+            <h2>Cadastro de vídeo</h2>
+            <form onSubmit={onSave}>
                 <div>
-                    <label>URL do Vídeo</label>
+                    <label>URL do vídeo</label>
                     <input
                         type="text"
                         placeholder="Digite a URL do vídeo"
                         required="required"
+                        value={url}
+                        onChange={ e => setUrl(e.target.value) }
+                        minLength="43"
+                        maxLength="43"
                     />
                 </div>
                 <div>
                     <label>Categoria</label>
-                    <select>
-                        <option value="-">Selecione uma Categoria</option>
+                    <select
+                        required="required"
+                        value={category}
+                        onChange={ e => setCategory(e.target.value) }
+                    >
+                        <option value="-">Selecione uma categoria</option>
                         { categories.map(item => {
-                            return <option value={item}>{item}</option>
+                            return <option key={item} value={item}>{item}</option>
                         }) }
                     </select>
                 </div>
                 <div>
                     <button>Cadastrar</button>
                 </div>
+                <div>
+                    { errors && <div className={styles.error}>{ errors }</div> }
+                </div>
             </form>
+           
         </section>
     );
 }
