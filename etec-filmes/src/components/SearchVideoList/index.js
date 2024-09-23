ab45fb04992b3fecf0e46e83d7597ab76a16a24f
GuiNakamuraC <guincarvalho08@gmail.com>
2024-09-22 21:10:04 -0300

    aula20

--- etec-filmes/src/components/SearchVideoList/index.js
+++ etec-filmes/src/components/SearchVideoList/index.js
@@ -1,6 +1,7 @@
 import styles from "./SearchVideoList.module.css";
 import VideoList from "../../components/VideoList";
-import {useState} from "react";
+import Loader from "../../components/Loader";
+import {useEffect, useState} from "react";
 
 // aqui vc filtra os videos por category ou title
 function filterVideos(videos, searchText){
@@ -11,6 +12,12 @@ function SearchVideoList({ videos }) {
 
     const [ searchText, setSearchText ] = useState('')
     const foundVideos = filterVideos(videos, searchText)
+
+    const [ loading, setLoading ] = useState(true)
+    useEffect(() => {
+        setTimeout(() => setLoading(false), 500)
+    }, [])
+
     
     return (
         <section className={styles.container}>
@@ -20,10 +27,13 @@ function SearchVideoList({ videos }) {
                 value={searchText}
                 onChange={event => setSearchText(event.target.value)}
             />
-            <VideoList
+            { 
+                loading ? <Loader /> :
+                <VideoList
                     videos={foundVideos}
                     emptyHeading={`Sem vídeos sobre "${searchText}"`}
-                /> 
+                />
+            }
         </section>
     );
 }
